3c99b60fe42f2b1ac246b6e2b4b7f5d3b0a962fd
ss0311 <soichiroseike@gmail.com>
2024-12-15 23:06:24 +0900

    エラー発生時にレスポンスをエラーにする

--- __tests__/__api__/schedule.test.ts
+++ __tests__/__api__/schedule.test.ts
@@ -19,4 +19,15 @@ describe("予定追加ユースケース", () => {
         expect(createdSchedule._startDatetime).toEqual("2024/12/11 12:00:00");
         expect(createdSchedule._endDatetime).toEqual("2024/12/11 13:00:00");
     });
+    it("予定の開始時間を終了時間と同じにしている場合、エラーが返却されること", async () => {
+        const request = supertest(server);
+        const response = await request.post("/schedule").send({
+            title: "詳細設計レビュー",
+            startDatetime: "2024/12/11 12:00:00",
+            endDatetime: "2024/12/11 12:00:00"
+        });
+
+        expect(response.status).toBe(400);
+        expect(response.body.errorMessage).toEqual("予定開始日時は予定終了日時より前に設定する必要があります startDatetime: 2024-12-11 12:00 endDatetime: 2024-12-11 12:00");
+    });
 });
--- presentation/rest/schduleController.ts
+++ presentation/rest/schduleController.ts
@@ -5,13 +5,20 @@ import { listSchdule } from '../../usecases/listSchedule';
 import { myContainer } from '../../application/inversify.config';
 import { ScheduleRepository } from '../../usecases/interface/scheduleRepository';
 import { TYPES } from '../../application/types';
+import { DomainError } from '../../error/domainError';
 
 const repository = myContainer.get<ScheduleRepository>(TYPES.ScheduleRepository)
 
 export const createSchdule = (request: any, response: any) => {
-    const schedule = convertSchedule(request);
-    createSchedule(repository, schedule)
-    response.status(200).end()
+    try {
+        const schedule = convertSchedule(request);
+        createSchedule(repository, schedule)
+        response.status(200).end()
+    } catch (e) {
+        if (e instanceof DomainError) {
+            response.status(400).send({ errorMessage: e.message })
+        }
+    }
 };
 
 export const allSchedules = (_: any, response: any) => {
